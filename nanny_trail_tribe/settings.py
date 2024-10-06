8e9834aa22aa6b39cfb5cf27cfe0cb786ad06525
Sam Larby <samlarby1997@hotmail.com>
2024-10-06 13:48:20 +0000

    creating trails database, edit trails page

--- nanny_trail_tribe/settings.py
+++ nanny_trail_tribe/settings.py
@@ -108,6 +108,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'trails' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trails',           
+        'USER': 'admin',         
+        'PASSWORD': '',          
+        'HOST': 'localhost',              
+        'PORT': '5432',
     }
 }
 
--- trails/views.py
+++ trails/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Trail
 
 def trails(request):
     """A view to return index page """
     return render(request, 'trails/trails.html')
+
+

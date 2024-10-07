ebc52efea33a74f278538da2624cdf7e690200ed
Sam Larby <samlarby1997@hotmail.com>
2024-10-07 19:37:47 +0000

    show users subscription status on their profile

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,2 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
+
+class SubscriptionOrder(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('active', 'Active'),
+        ('cancelled', 'Cancelled'),
+        ('expired', 'Expired'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #Links to the user
+    created_at = models.DateTimeField(auto_now_add=True) #Get the date of the subscription order
+    start_date = models.DateField(default=timezone.now) #The start of the subscription
+    end_date = models.DateField() # The end date of the subscription
+    amount = models.DecimalField(max_digits=10, decimal_places=2) # price of subscription
+    status = models.Charfield(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.status} ({self.start_date} to {self.end_date})"
--- subscribe/views.py
+++ subscribe/views.py
@@ -2,14 +2,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from datetime import timedelta
 from profiles.models import UserProfile
+from .models import SubscriptionOrder
 
 @login_required
 def subscribe(request):
-    user_profile = request.user.profile
-    user_profile.subscription_active = True
+    user_profile = request.user.UserProfile
+
+    # if user is ot subscribed create an order for the user
+    if not user_profile.subscription_active:
+        # duration and price of subscription, this will be 1 month (30 days)
+        start_date = timezone.now().date()
+        end_date = start_date + timedelta(days=30) 
+        amount = 10.00 
+    
+    # create the subscription order 
+    subscription_order = SubscriptionOrder.objects.create(
+        user=request.user,
+        start_date=start_date,
+        end_date=end_date,
+        amount=amount,
+        status='active'
+    )
+
+    # show updates on users profile
+    user_profile.subscription_status = True
+    user_profile.current_subscription = subscription_order
     user_profile.save()
-    return redirect('subscription_status')
+
+return redirect('subscription_status')
+
+
 
 @login_required
 def unsubscribe(request):
